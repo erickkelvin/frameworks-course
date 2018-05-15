d473092aa940d534283417a1bb1e1e2ee6d6eb38
Erick Kelvin <erickkelvin@hotmail.com>
2018-05-15 20:43:46 -0300

    reorganizing code and callbacks

--- project1/controllers/state_controller.js
+++ project1/controllers/state_controller.js
@@ -2,66 +2,58 @@
 var mongoose = require('mongoose');
 var State = mongoose.model('States');
 
-exports.list = function() {
+exports.list = () => {
   var promise = State.find();
-  return promise.then( (result) => {
+  return promise.then((result) => {
     return result;
   })
-  .catch( (err) => {
+  .catch((err) => {
     return err;
   });
 };
 
-exports.create = function(data) {
-  var content = new State(data);
-  content.save(function(err){
-    if(err){
-      console.log(err);
-      return err;
-    }
-  });
-};
-
-exports.read = function(id) {
+exports.read = (id) => {
   var promise = State.findById(id);
-  return promise.then( (result) => {
+  return promise.then((result) => {
     return result;
   })
-  .catch( (err) => {
+  .catch((err) => {
     return err;
   });
 };
 
-exports.delete = function(id) {
-  State.findByIdAndRemove(id, function(err) {
-    if (err){
-      console.log(err);
-      return err;
-    }
+exports.create = (data, success, error) => {
+  var content = new State(data);
+  content.save((err) => {
+    return err ? error(err) : success();
   });
 };
 
-exports.update = function(id, data) {
-  State.findByIdAndUpdate(id, {$set: data}, {new: true}, function(err, result) {
-    if (err){
-      console.log(err);
-      return err;
-    }
+exports.update = (id, data, success, error) => {
+  State.findByIdAndUpdate(id, {$set: data}, {new: true}, (err, result) => {
+    return err ? error(err) : success(result);
   });
 };
 
-exports.read_by_criteria = function(filters) {
+exports.delete = (id, success, error) => {
+  State.findByIdAndRemove(id, (err) => {
+    return err ? error(err) : success();
+  });
+};
+
+exports.read_by_criteria = (filters) => {
   var query = State.find();
 
   for (var i = 0; i < filters.length; i++) {
     query.where(filters[i].fieldName).equals(filters[i].value)
   }
+  
   var promise = query.exec();
 
-  return promise.then( (result) => {
+  return promise.then((result) => {
     return result;
   })
-  .catch( (err) => {
+  .catch((err) => {
     return err;
   });
 };
