3f283b0c39927466d40867256f297e026d8c9af6
Zedequias Santos <zedequiassantoss@alu.ufc.br>
2018-05-15 21:48:16 -0300

    add log

--- project1/app.js
+++ project1/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -29,6 +30,12 @@ db.on('error', console.error.bind(console, 'Error connecting to MongoDB!'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+app.use(logger(
+  '[:date[web]] :remote-addr - :method :url (:status) - :user-agent - :response-time ms',
+  { stream: accessLogStream }
+));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
